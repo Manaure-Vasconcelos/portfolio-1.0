4c5f683afb4824b735f12f8da137de22b536fda2
manaure <manaurevasc97@gmail.com>
2024-11-01 13:45:43 -0300

    style: refactory carouselprojects

--- src/components/carouselProjects.tsx
+++ src/components/carouselProjects.tsx
@@ -35,18 +35,18 @@ export function CarouselPlugin() {
         {Array.from({ length: 3 }).map((_, index) => (
           <CarouselItem
             key={index}
-            className="w-full h-[250px] md:h-full flex items-center justify-between"
+            className="w-full h-[600px] md:h-[550px] flex items-center justify-center"
           >
-            <div className="flex w-full h-full">
+            <div className="flex flex-col justify-center border items-center w-full h-full">
               {/* Imagem do lado esquerdo */}
-              <div className="w-1/2 h-full flex items-center justify-center">
+              <div className="w-11/12 h-2/3 flex items-center justify-center">
                 <img
                   src={`https://images.unsplash.com/photo-1719253480609-579ad1622c65?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHByb3JqZXRvcyUyMGRldnxlbnwwfHwwfHx8MA%3D%3D`}
                   className="w-11/12 h-full object-cover"
                 />
               </div>
               {/* Título e Descrição do lado direito */}
-              <div className="w-1/2 flex flex-col justify-between px-3 md:p-5">
+              <div className="w-full h-1/3 flex flex-col justify-between px-3 md:p-5">
                 <div>
                   <h3 className="text-2xl md:text-4xl font-bold mb-3">
                     Título {index + 1}
